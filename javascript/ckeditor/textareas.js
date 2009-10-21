78e317bb51b39e6127d9a3b4e37a52cfd04f5e18
David Evans <dave@orman.(none)>
2009-10-21 16:19:31 +0100

    Set editor height to original textarea height
    For some reason, CKeditor doesn't do this automatically for height, although it does for width.

--- javascript/ckeditor/textareas.js
+++ javascript/ckeditor/textareas.js
@@ -1,13 +1,16 @@
 //= requires "editor"
 //= requires <mootools/DomReady>
 //= requires <mootools/Selectors>
+//= requires <mootools/Element.Dimensions>
 
 // Replace all textreas with class 'richtexteditor' with ckeditor instances
 window.addEvent('domready', function() {
 	document.getElements('textarea.richtexteditor').each(function(el) {
+		var size = el.getSize();
 		CKEDITOR.replace(el, {
 			// Turn off custom config so it doesn't attempt to load config file
-			customConfig : '' 
+			customConfig : '',
+			height: size.y 
 		});
 	});
 });
